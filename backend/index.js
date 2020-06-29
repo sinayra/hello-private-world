271057446355d91a32c4c0dd116a98728d0e0bd5
Sinayra <nayra@hotmail.co.jp>
2020-06-29 14:27:50 -0300

    Simple app with two containers

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3333;
+
+const allowlist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: allowlist,
+  methods: "GET",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.json({ message: "Hello Private World!" });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
