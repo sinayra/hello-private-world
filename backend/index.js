4500936b9aed40aae6934c1e035e9696efb4056f
Sinayra <nayra@hotmail.co.jp>
2020-07-02 15:22:40 -0300

    Add not reachable route

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,27 @@ const app = express();
 const cors = require("cors");
 const port = 3333;
 
-const allowlist = ["http://localhost:3000"];
+const allowlist = ["http://localhost"];
+
 const corsOptions = {
   origin: allowlist,
   methods: "GET",
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
+
 app.get("/api", (req, res) => {
   console.log("GET /api");
   res.json({ message: "Hello Private World!" });
+});
+
+app.get("/not-reachable", (req, res) => {
+  console.log("GET /not-reachable");
+  res.json({ message: "Hello Secret World!" });
 });
 
 app.listen(port, () =>
